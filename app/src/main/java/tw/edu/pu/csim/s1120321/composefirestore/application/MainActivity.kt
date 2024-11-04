51ec8a1a3e46f733ef1124da405f05d617147211
s1120321 <yieewenhong@gmail.com>
2024-11-04 15:36:14 +0800

    v4_密碼輸入

--- app/src/main/java/tw/edu/pu/csim/s1120321/composefirestore/application/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1120321/composefirestore/application/MainActivity.kt
@@ -60,6 +60,7 @@ class MainActivity : ComponentActivity() {
 fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("洪藝文")}
     var userWeight by remember { mutableStateOf(3800)}
+    var userPassword by remember { mutableStateOf("")}
 
     Column {
         TextField(
@@ -83,6 +84,19 @@ fun Birth(m: Modifier){
             keyboardOptions = KeyboardOptions
                 (keyboardType = KeyboardType.Number)
         )
-        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克" + "\n密碼：$userPassword")
+
     }
 }
